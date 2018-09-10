4c5dc55d4488dbf1c7de6a66d940ae037b926c78
uwoer <uwoerla@163.com>
2018-09-10 22:20:53 +0800

    sacla learning

--- scala-learning/src/main/scala/com/uwola/scala/clazz/ClassTestApp.scala
+++ scala-learning/src/main/scala/com/uwola/scala/clazz/ClassTestApp.scala
@@ -5,7 +5,7 @@ import com.alibaba.fastjson.JSON
 /**
   * Created by uwoer on 2018/9/10.
   */
-object App {
+object ClassTestApp {
   def main(args: Array[String]): Unit = {
     val p = new Person()
     // setter
@@ -31,20 +31,20 @@ object App {
     // setter方法
     p2.setAge(29)
     // 通过getter方法打印
-    println("p2 name is "+p2.getName+" age is "+p2.getAge )
+    println("p2 name is "+p2.getName+" age is "+p2.getAge)
+
+    // 对象转json json转对象
     val jsonStr = JSON.toJSON(p2).toString
     println(jsonStr)
     val json2p = JSON.parseObject(jsonStr,classOf[Person])
-    println("json2p name is "+json2p.name+" age is "+json2p.age )
+    println("＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝")
+    println("json2p name is "+json2p.name+" age is "+json2p.age)
     var json2p1 = JSON.parseObject(jsonStr,classOf[Person1])
-    println("json2p1 name is "+json2p1.name+" age is "+json2p1.age )
+    println("json2p1 name is "+json2p1.name+" age is "+json2p1.age)
     val json2p2 = JSON.parseObject(jsonStr,classOf[Person2])
-    println("json2p2 name is "+json2p2.name+" age is "+json2p2.age )
-    println("json2p2 name is "+json2p2.getName+" age is "+json2p2.getAge )
+    println("json2p2 name is "+json2p2.name+" age is "+json2p2.age)
+    println("json2p2 name is "+json2p2.getName+" age is "+json2p2.getAge)
 
-    val json = "{\"name\":\"uwoer\"}"
-    json2p1 = JSON.parseObject(json,classOf[Person1])
-    println("json2p1 name is "+json2p1.name+" age is "+json2p1.age )
   }
 
 }
--- scala-learning/src/main/scala/com/uwola/scala/clazz/Person3.scala
+++ scala-learning/src/main/scala/com/uwola/scala/clazz/Person3.scala
@@ -1,7 +1,8 @@
 package com.uwola.scala.clazz
 
 /**
-  *  主constructor是与类名放在一起
+  *  主constructor与类名放在一起
+  *  参数有默认值
   * Created by uwoer on 2018/9/10.
   */
 class Person3(val name:String = "嘟嘟", val age:Int = 1) {
